331c43ae1265c11a08d715fd4167baeeb55a1740
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-05-03 23:57:25 +0200

    Added select and delete functionality

---
+++ Assets/EquipmentSelectionTool.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSelectionTool : PointerSelector
+{
+    [SerializeField] GameObject selectionPointerPrefab;
+    GameObject selectionPointerInstance;
+    GameObject pickedObject;
+
+    protected override void Update()
+    {
+        base.Update();
+        if (_selection != null)
+        {
+            Debug.Log(_selection.gameObject.name);
+
+            if (Input.GetMouseButtonDown(0))
+                pickedObject = _selection.gameObject;            
+        }
+
+        if (pickedObject != null)
+        {
+            if (selectionPointerInstance == null)
+                selectionPointerInstance = Instantiate(selectionPointerPrefab, pickedObject.transform);
+            selectionPointerInstance.transform.position = pickedObject.transform.position + Vector3.up * pickedObject.GetComponent<BoxCollider>().size.y * 1.2f;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                pickedObject = null;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                Destroy(pickedObject);
+                pickedObject = null;
+            }
+        }
+        else
+        {
+            Destroy(selectionPointerInstance);
+            selectionPointerInstance = null;
+        }
+    }
+    
+}
